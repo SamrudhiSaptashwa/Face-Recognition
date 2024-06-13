c18098a8e3a8b94589f12de487c90e92211e58f9
Samrudhi Mahesh Saptashwa <116869332+SamrudhiSaptashwa@users.noreply.github.com>
2024-06-13 13:21:34 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,15 @@
+sudo docker run \
+    --runtime nvidia \
+    --rm \
+    --net host \
+    --env DISPLAY=${DISPLAY} \
+    --env PULSE_SERVER=unix:/run/user/${UID}/pulse/native \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix \
+    --volume ~/.Xauthority:/root/.Xauthority \
+    --volume /home/jetbot/face-check/src:/face-check/src \
+    --volume /home/jetbot/face-check/data:/face-check/data \
+    --volume /tmp/argus_socket:/tmp/argus_socket \
+    --volume /run/user/${UID}/pulse/native:/run/user/${UID}/pulse/native \
+    --device /dev/video0 \
+    --workdir /face-check/src \
+    take5553/face-check:jp5.0 python3 main.py
